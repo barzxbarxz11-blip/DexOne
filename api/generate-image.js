b7ac5b49d1b184bcb243af891a966c024f4a720d
barzxbarxz11-blip <barzxbarxz11@gmail.com>
2026-05-24 17:19:13 +0700

    Change GoogleGenAI API key
    Updated API key for GoogleGenAI instantiation.

--- api/generate-image.js
+++ api/generate-image.js
@@ -1,6 +1,6 @@
 import { GoogleGenAI } from '@google/genai';
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyMasingMasingApiKeyAndaDiSini" });
+const ai = new GoogleGenAI({ apiKey: "AIzaSyDwraKSpVwKFfTuZaTNlX8COYGYiAs7_tE" });
 
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
